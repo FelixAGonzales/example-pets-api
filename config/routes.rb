eb8e0519cf956393ebdd6a19eafd8f08321411df
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-09-11 20:43:08 -0400

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-
+  get "users" => "users#index"
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   # Defines the root path route ("/")
